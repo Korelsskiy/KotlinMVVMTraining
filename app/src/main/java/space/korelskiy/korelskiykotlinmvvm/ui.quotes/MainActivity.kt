7e3751cc698df08a5e06cf14df0164f991a76ae2
Korelsskiy <68390165+Korelsskiy@users.noreply.github.com>
2020-11-04 04:51:43 +0300

    added some directories

--- app/src/main/java/space/korelskiy/korelskiykotlinmvvm/ui.quotes/MainActivity.kt
+++ app/src/main/java/space/korelskiy/korelskiykotlinmvvm/ui.quotes/MainActivity.kt
@@ -1,7 +1,8 @@
-package space.korelskiy.korelskiykotlinmvvm
+package space.korelskiy.korelskiykotlinmvvm.ui.quotes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import space.korelskiy.korelskiykotlinmvvm.R
 
 class QuotesActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
